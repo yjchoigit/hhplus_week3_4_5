2226cfb535033eaa4fd8b966cf31c0e92e07181b
yjchoi <yjchoi@forbiz.co.kr>
2024-07-02 13:52:07 +0900

    initial commit

---
+++ src/test/java/com/hhplus/hhplus_week3/HhplusWeek3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.hhplus_week3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusWeek3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
