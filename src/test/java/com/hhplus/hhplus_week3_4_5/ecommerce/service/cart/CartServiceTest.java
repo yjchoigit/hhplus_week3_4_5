f8ed8f712a1823cdec34095b6396f2d8fbf61827
yjchoigit <gmcadeau@gmail.com>
2024-07-17 21:46:30 +0900

    test: 장바구니 단위 테스트 생성

---
+++ src/test/java/com/hhplus/hhplus_week3_4_5/ecommerce/service/cart/CartServiceTest.java
@@ -0,0 +1,127 @@
+package com.hhplus.hhplus_week3_4_5.ecommerce.service.cart;
+
+import com.hhplus.hhplus_week3_4_5.ecommerce.domain.buyer.entity.Buyer;
+import com.hhplus.hhplus_week3_4_5.ecommerce.domain.cart.entity.Cart;
+import com.hhplus.hhplus_week3_4_5.ecommerce.domain.cart.repository.CartRepository;
+import com.hhplus.hhplus_week3_4_5.ecommerce.domain.product.ProductEnums;
+import com.hhplus.hhplus_week3_4_5.ecommerce.domain.product.entity.Product;
+import com.hhplus.hhplus_week3_4_5.ecommerce.domain.product.entity.ProductOption;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CartServiceTest {
+
+    @InjectMocks
+    private CartServiceImpl cartServiceImpl;
+
+    @Mock
+    private CartRepository cartRepository;
+
+    private Buyer buyer;
+    private Cart cart;
+    private Product product;
+    private ProductOption productOption;
+
+    @BeforeEach
+    void setUp() {
+        // 회원 등록
+        buyer = new Buyer(1L, "홍길동");
+        // 상품 등록
+        product = Product.builder()
+                .productId(1L)
+                .name("운동화")
+                .type(ProductEnums.Type.OPTION)
+                .price(3000)
+                .useYn(true)
+                .delYn(false)
+                .build();
+        // 상품 옵션 등록
+        List<ProductOption> options = new ArrayList<>();
+        options.add(new ProductOption(1L, product, ProductEnums.OptionType.BASIC, "색깔", "빨강", 3100, true));
+        productOption = options.get(0);
+
+        // 장바구니 등록
+        cart = new Cart(1L, buyer.getBuyerId(), product.getProductId(), productOption.getProductOptionId(), 10);
+    }
+
+    @Test
+    @DisplayName("장바구니 목록 조회 성공")
+    void findCartList_success(){
+        // given
+        Long buyerId = 1L;
+
+        // when
+        when(cartRepository.findCartListByBuyerId(buyerId)).thenReturn(List.of(cart));
+
+        // then
+        List<Cart> result = cartServiceImpl.findCartList(buyerId);
+        assertFalse(result.isEmpty());
+    }
+
+    @Test
+    @DisplayName("장바구니 추가 성공")
+    void addCart_success(){
+        // given
+        Long buyerId = 1L;
+
+        // when
+        when(cartRepository.save(any(Cart.class))).thenReturn(cart);
+
+        // then
+        Cart result = cartServiceImpl.addCart(Cart.builder()
+                .buyerId(buyerId)
+                .productId(1L)
+                .productOptionId(1L)
+                .buyCnt(10)
+                .build());
+
+        assertEquals(result.getBuyCnt(),10);
+    }
+
+    @Test
+    @DisplayName("장바구니 삭제 성공")
+    void delCart_success(){
+        // given
+        Long buyerId = 1L;
+
+        // when
+        when(cartRepository.findCartListByBuyerIdAndCartIdList(any(), any())).thenReturn(List.of(cart));
+
+        // then
+        boolean result = cartServiceImpl.delCart(buyerId, List.of(1L));
+
+        assertTrue(result);
+        verify(cartRepository).delete(any());
+    }
+
+    @Test
+    @DisplayName("장바구니 삭제 실패 - 장바구니 정보가 없을 때")
+    void delCart_success_no_product_fail(){
+        // given
+        Long buyerId = 1L;
+
+        // when
+        when(cartRepository.findCartListByBuyerIdAndCartIdList(any(), any())).thenReturn(new ArrayList<>());
+
+        // then
+        assertThrows(IllegalArgumentException.class, ()-> {
+            cartServiceImpl.delCart(buyerId, List.of(1L));
+        });
+        verify(cartRepository, never()).delete(any());
+    }
+
+}
