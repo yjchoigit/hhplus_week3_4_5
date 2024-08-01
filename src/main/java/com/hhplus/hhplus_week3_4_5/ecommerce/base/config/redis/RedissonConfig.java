9ac9f86da5be2bdc459beb06325b67b13df5208f
yjchoigit <gmcadeau@gmail.com>
2024-08-01 17:53:46 +0900

    perf: 레디스 log 추가

--- src/main/java/com/hhplus/hhplus_week3_4_5/ecommerce/base/config/redis/RedissonConfig.java
+++ src/main/java/com/hhplus/hhplus_week3_4_5/ecommerce/base/config/redis/RedissonConfig.java
@@ -1,5 +1,6 @@
 package com.hhplus.hhplus_week3_4_5.ecommerce.base.config.redis;
 
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class RedissonConfig {
 
     @Bean
@@ -14,6 +16,9 @@ public class RedissonConfig {
         Config config = new Config();
         config.useSingleServer()
                 .setAddress("redis://localhost:6379");
+
+        log.info("Connecting to Redis at: {}", config.useSingleServer().getAddress());
+
         return Redisson.create(config);
     }
 }
